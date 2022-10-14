e8fffa2cca20426ff4119bde515b55fb247fa9c2
alu10656117 <aluopr6117@ieselcaminas.org>
2022-10-14 18:57:41 +0200

    First commit

---
+++ src/Library.java
@@ -0,0 +1,10 @@
+public class Library {
+    public static void main(String[] args) {
+
+        Author author = new Author("Ruben Hidalgo", "rubencito@gmail.com", 'M');
+        System.out.println(author);
+        Book book = new Book("x", author, 35, 10);
+        Book b2 = new Book("Foundation",
+                new Author("Isaak Newtown", "gg@gmail.com,", 'M'), 30,14);
+    }
+}
